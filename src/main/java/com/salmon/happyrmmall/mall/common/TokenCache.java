28d65a6bc3d3343e191931773e1b1c3520b74ff0
salmon1802 <757635525@qq.com>
2022-01-13 19:07:02 +0800

    第二次提交

--- src/main/java/com/salmon/happyrmmall/mall/common/TokenCache.java
+++ src/main/java/com/salmon/happyrmmall/mall/common/TokenCache.java
@@ -5,8 +5,6 @@ import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -19,7 +17,10 @@ public class TokenCache {
     public static final String TOKEN_PREFIX = "token_";
 
     //LRU算法,即当最大值超过一万时会使用最小使用算法进行清除
-    private static LoadingCache<String,String> localCache = CacheBuilder.newBuilder().initialCapacity(1000).maximumSize(10000).expireAfterAccess(12, TimeUnit.HOURS)
+    private static LoadingCache<String,String> localCache = CacheBuilder.newBuilder()
+            .initialCapacity(1000)
+            .maximumSize(10000)
+            .expireAfterAccess(12, TimeUnit.HOURS)
             .build(new CacheLoader<String,String>(){
                 //默认的数据加载实现，当调用get取值的时候，如果key没有对应的值，就调用这个方法进行加载。
                 public String load(String s) throws Exception{
